b2ff0102beecca4c38dab0b70dca2a37b35af751
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-06-15 22:24:22 +0200

    Initial commit

---
+++ Assets/Tiles/CoordinateLabel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabel : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpadeObjectName();
+        }
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x); //because this script is on a child -- sprawdzić potem Convert.toint32 - wg mnie nie używać żeby nie korzystać niepotrzebnie z using System
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z); 
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void UpadeObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
