4cd8b7c07ace3bc2d43863dc52c5b43af81b47b9
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-06-16 23:18:58 +0200

    Creating path ,and moving enemy

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-
+    [SerializeField] [Range(0f, 5f)] float speed = 1f;
     
     void Start()
     {
-        PrintWaypointName();
+        //Debug.Log("Start here");
+        StartCoroutine(FollowPath());
+        //Debug.Log("Finishing start"); - to see how yield works
     }
 
-    void PrintWaypointName()
+    IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
         {
-            Debug.Log(waypoint.name);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+            
+            while(travelPercent <1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+
+            //Przeskakiwanie po Tilesach    
+            //transform.position = waypoint.transform.position;
+            //Debug.Log(waypoint.name); - to see how yield works
+            //yield return new WaitForSeconds(waitTime);
         }
     }
 }
